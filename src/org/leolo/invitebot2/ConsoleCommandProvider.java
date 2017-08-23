6b103cb137423be85f2db0bee4a8547695fcb36a
LO Kam Tao Leo <leolo@leolo.org>
2017-08-23 20:43:37 +0800

    Allow alias reload

--- src/org/leolo/invitebot2/ConsoleCommandProvider.java
+++ src/org/leolo/invitebot2/ConsoleCommandProvider.java
@@ -1,10 +1,15 @@
 package org.leolo.invitebot2;
 
 import org.leolo.invitebot2.annotation.ConsoleCommand;
+import org.leolo.invitebot2.db.CommandAliasDao;
+import org.leolo.invitebot2.db.DBManager;
 import org.pircbotx.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConsoleCommandProvider {
-
+	Logger logger = LoggerFactory.getLogger(ConsoleCommandProvider.class);
+	
 	@ConsoleCommand(pmOnly = false, name = "ping")
 	public ConsoleCommandResult ping(String line, User source){
 		ConsoleCommandResult ccr = new ConsoleCommandResult();
@@ -16,6 +21,17 @@ public class ConsoleCommandProvider {
 	public ConsoleCommandResult echo(String line, User source){
 		ConsoleCommandResult ccr = new ConsoleCommandResult();
 		ccr.println(line.substring(5));
+		return ccr;
+	}
+	
+	@ConsoleCommand(pmOnly = false, name = "aliasreload")
+	public ConsoleCommandResult reloadAlias(String line, User source){
+		ConsoleCommandResult ccr = new ConsoleCommandResult();
+		DBManager dbMan = DBManager.getInstance();
+		CommandAliasDao cadao = dbMan.getCommandAliasDao();
+		Console.commandAlias = cadao.getAll();
+		ccr.println("Alias reloaded");
+		logger.info(Console.commandAlias.toString());
 		return ccr;
 	}
 	
